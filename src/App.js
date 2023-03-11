6cecd0ab0b436f1272aab51fe89c73099653dba0
Azeez Roheem <mediatoor@gmail.com>
2023-03-11 10:16:49 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import InputShortner from './InputShortner';
+
+function App() {
+  return (
+      <header className="container">
+        <p>
+          <InputShortner />
+        </p>
+      </header>
+  );
+}
+
+export default App;
---
+++ src/InputShortner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const InputShortner = () => {
+    return (
+        <div className='inputContainer'>
+            <h1>URL <span>Shortner</span></h1>
+            <div>
+            <input type="text" placeholder='Paste a link to shorten it' />
+            <button>shorten</button>
+            </div>
+        </div>
+        
+    )
+}
+
+export default InputShortner;
